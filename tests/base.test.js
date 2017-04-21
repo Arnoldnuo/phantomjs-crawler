dd5a44f16bd6875af7c0ec19c22c0b1c46482bfb
linuo <linuo@baidu.com>
2017-04-21 12:12:18 +0800

    添加抓取的随机事件

--- tests/base.test.js
+++ tests/base.test.js
@@ -4,12 +4,19 @@ var Crawler = require('../lib/crawler');
 var expect = require('chai').expect;
 var baseUri = 'https://movie.douban.com/subject/1292052/questions/164/?from=subject';
 
+let a = new Date().getTime();
+
 describe('phantom crawler', function() {
     it('should excute js', async function(done) {
         let c = new Crawler({
             callback: async(option) => {
-                console.log(option);
-            }
+                // console.log(option);
+                console.log(new Date().getTime() - a);
+                a = new Date().getTime();
+            },
+	    minRateLimit: 1000,
+	    maxRateLimit: 9000,
+	    randomRateLimit: true
         });
         await c.queue(baseUri);
         await c.ready();
